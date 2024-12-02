3f3cf414c070f8e7c94a614f196df3326de96115
ayc0735work <ayc0735@unes.co.kr>
2024-12-02 16:42:12 +0900

    실시간 비교 영역 관리 모달 개선

--- BE/ui/components/advanced/compare_area_dialog.py
+++ BE/ui/components/advanced/compare_area_dialog.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import (QDialog, QVBoxLayout, QHBoxLayout, 
-                              QLabel, QPushButton, QFrame)
+                              QLabel, QPushButton, QFrame, QLineEdit,
+                              QGridLayout, QWidget)
 from PySide6.QtCore import Qt
+from ..process_selector.process_selector_dialog import ProcessSelectorDialog
 
 class CompareAreaDialog(QDialog):
     def __init__(self, parent=None):
@@ -10,7 +12,7 @@ class CompareAreaDialog(QDialog):
     def init_ui(self):
         """UI 초기화"""
         self.setWindowTitle("실시간 비교 영역 관리")
-        self.setFixedSize(600, 400)
+        self.setFixedSize(600, 500)
         
         layout = QVBoxLayout()
         layout.setSpacing(12)
@@ -59,6 +61,94 @@ class CompareAreaDialog(QDialog):
         
         layout.addWidget(process_frame)
         
+        # 캡처된 영역 정보
+        self.captured_area_info = QLabel("캡처된 영역이 없습니다")
+        self.captured_area_info.setWordWrap(True)
+        layout.addWidget(self.captured_area_info)
+        
+        # 캡처 영역 좌표 입력 필드들을 포함할 수평 레이아웃
+        coordinates_container = QHBoxLayout()
+        
+        # 캡처 영역 좌표 입력 필드들
+        coordinates_frame = QFrame()
+        coordinates_frame.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        coordinates_layout = QGridLayout()
+        coordinates_layout.setSpacing(10)
+        coordinates_layout.setContentsMargins(10, 10, 10, 10)
+        
+        # 좌상단 좌표
+        top_left_layout = QHBoxLayout()
+        top_left_layout.addWidget(QLabel("좌상단:"))
+        self.top_left_x = QLineEdit()
+        self.top_left_x.setReadOnly(True)
+        self.top_left_x.setPlaceholderText("X")
+        self.top_left_x.setFixedWidth(60)
+        top_left_layout.addWidget(self.top_left_x)
+        self.top_left_y = QLineEdit()
+        self.top_left_y.setReadOnly(True)
+        self.top_left_y.setPlaceholderText("Y")
+        self.top_left_y.setFixedWidth(60)
+        top_left_layout.addWidget(self.top_left_y)
+        top_left_widget = QWidget()
+        top_left_widget.setLayout(top_left_layout)
+        coordinates_layout.addWidget(top_left_widget, 0, 0)
+        
+        # 우상단 좌표
+        top_right_layout = QHBoxLayout()
+        top_right_layout.addWidget(QLabel("우상단:"))
+        self.top_right_x = QLineEdit()
+        self.top_right_x.setReadOnly(True)
+        self.top_right_x.setPlaceholderText("X")
+        self.top_right_x.setFixedWidth(60)
+        top_right_layout.addWidget(self.top_right_x)
+        self.top_right_y = QLineEdit()
+        self.top_right_y.setReadOnly(True)
+        self.top_right_y.setPlaceholderText("Y")
+        self.top_right_y.setFixedWidth(60)
+        top_right_layout.addWidget(self.top_right_y)
+        top_right_widget = QWidget()
+        top_right_widget.setLayout(top_right_layout)
+        coordinates_layout.addWidget(top_right_widget, 0, 1)
+        
+        # 좌하단 좌표
+        bottom_left_layout = QHBoxLayout()
+        bottom_left_layout.addWidget(QLabel("좌하단:"))
+        self.bottom_left_x = QLineEdit()
+        self.bottom_left_x.setReadOnly(True)
+        self.bottom_left_x.setPlaceholderText("X")
+        self.bottom_left_x.setFixedWidth(60)
+        bottom_left_layout.addWidget(self.bottom_left_x)
+        self.bottom_left_y = QLineEdit()
+        self.bottom_left_y.setReadOnly(True)
+        self.bottom_left_y.setPlaceholderText("Y")
+        self.bottom_left_y.setFixedWidth(60)
+        bottom_left_layout.addWidget(self.bottom_left_y)
+        bottom_left_widget = QWidget()
+        bottom_left_widget.setLayout(bottom_left_layout)
+        coordinates_layout.addWidget(bottom_left_widget, 1, 0)
+        
+        # 우하단 좌표
+        bottom_right_layout = QHBoxLayout()
+        bottom_right_layout.addWidget(QLabel("우하단:"))
+        self.bottom_right_x = QLineEdit()
+        self.bottom_right_x.setReadOnly(True)
+        self.bottom_right_x.setPlaceholderText("X")
+        self.bottom_right_x.setFixedWidth(60)
+        bottom_right_layout.addWidget(self.bottom_right_x)
+        self.bottom_right_y = QLineEdit()
+        self.bottom_right_y.setReadOnly(True)
+        self.bottom_right_y.setPlaceholderText("Y")
+        self.bottom_right_y.setFixedWidth(60)
+        bottom_right_layout.addWidget(self.bottom_right_y)
+        bottom_right_widget = QWidget()
+        bottom_right_widget.setLayout(bottom_right_layout)
+        coordinates_layout.addWidget(bottom_right_widget, 1, 1)
+        
+        coordinates_frame.setLayout(coordinates_layout)
+        coordinates_container.addWidget(coordinates_frame)
+        coordinates_container.addStretch()
+        layout.addLayout(coordinates_container)
+        
         # 캡처 이미지 표시 영역
         capture_frame = QFrame()
         capture_frame.setStyleSheet("""
@@ -68,7 +158,7 @@ class CompareAreaDialog(QDialog):
                 border-radius: 4px;
             }
         """)
-        capture_frame.setMinimumHeight(250)
+        capture_frame.setMinimumHeight(150)
         
         # 캡처 이미지가 없을 때 표시할 안내 텍스트
         guide_label = QLabel("캡처된 이미지가 없습니다.\n영역을 지정해주세요.")
@@ -153,16 +243,19 @@ class CompareAreaDialog(QDialog):
         # 시그널 연결
         self.process_select_btn.clicked.connect(self._select_process)
         self.process_reset_btn.clicked.connect(self._reset_process)
+        self.cancel_btn.clicked.connect(self.reject)  # 취소 버튼에 reject 연결
         
     def _select_process(self):
         """프로세스 선택"""
-        # TODO: 프로세스 선택 다이얼로그 표시
-        # 임시로 프로세스가 선택되었다고 가정
-        self.process_info.setText("선택된 프로세스 [PID: 1234] MapleStory - 메이플스토리")
-        self.capture_btn.setEnabled(True)
+        dialog = ProcessSelectorDialog(self)
+        if dialog.exec_():
+            selected_process = dialog.selected_process
+            if selected_process:
+                process_info_text = f"선택된 프로세스 [PID: {selected_process['pid']}] {selected_process['name']} - {selected_process['title']}"
+                self.process_info.setText(process_info_text)
+                self.capture_btn.setEnabled(True)
         
     def _reset_process(self):
         """프로세스 초기화"""
         self.process_info.setText("선택된 프로세스 없음")
         self.capture_btn.setEnabled(False)
-        
